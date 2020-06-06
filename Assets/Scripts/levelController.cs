e6cb65b3b2daffecb10d9cbfdc3a280aec9bf037
Dmitry <normiqw@mail.ru>
2020-06-06 15:32:44 +0300

    add tasks on level

--- Assets/Scripts/levelController.cs
+++ Assets/Scripts/levelController.cs
@@ -9,7 +9,10 @@ using UnityEngine.UI;
 public class level
 {
 #if UNITY_EDITOR
+    public string Name;
     public int levelNumber;
+    public int tasksOnLevel;
+    public int propsOnTaskCount;
     public List<prop> propsOnLevel;
 #endif
 }
@@ -43,12 +46,20 @@ public class levelController : MonoBehaviour
     public GameObject currentSpot;
     public GameObject levelLabel;
     public GameObject winPanel;
+    public GameObject taskStatusSpot;
+    public GameObject taskStatusPrefab;
     public int currentLevelIndex;
     public levelsList levelsList;
-    private level _currentLevel;
-    private List<prop> _propsOnLevel;
-    private Sprite _rightSilhouette;
-    private RuntimePlatform _platform;
+    
+    [Header("Debug")]
+    public level _currentLevel;
+    public List<GameObject> _tasksStatusObjects = new List<GameObject>();
+    public int _taskCurrentNumber;
+    public int _loseTasksOnLevel;
+    public List<prop> _propsOnLevel;
+    public List<prop> _propsOnTask = new List<prop>();
+    public Sprite _rightSilhouette;
+    public RuntimePlatform _platform;
 
     // Start is called before the first frame update
     private void Start()
@@ -68,9 +79,21 @@ public class levelController : MonoBehaviour
     {
         levelLabel.GetComponent<Text>().text = "Level " + _currentLevel.levelNumber;
         _propsOnLevel = _currentLevel.propsOnLevel;
+        _taskCurrentNumber = 0;
+        _loseTasksOnLevel = 0;
         shuffle(_propsOnLevel);
-        generatePropsOnHiddenSpots(_propsOnLevel);
+        generateTasksStatusOnLevel(_currentLevel.tasksOnLevel);
+        generatePropsOnHiddenSpots(_propsOnLevel, _currentLevel.propsOnTaskCount);
         generatePropsCurrent();
+    }
+    
+    private void generateTasksStatusOnLevel(int tasksOnLevel)
+    {
+        for (var i = 0; i < tasksOnLevel; i++)
+        {
+            var taskObject = Instantiate(taskStatusPrefab, taskStatusSpot.transform) as GameObject;
+            _tasksStatusObjects.Add(taskObject);
+        }
     }
 
     private static void shuffle<T>(List<T> list)
@@ -85,25 +108,25 @@ public class levelController : MonoBehaviour
         }
     }
 
-    private void generatePropsOnHiddenSpots(List<prop> propsList)
+    private void generatePropsOnHiddenSpots(IReadOnlyList<prop> propsList, int propsOnTaskCount)
     {
-        for (var i = 0; i < propsList.Count; i++)
+        for (var i = 0; i < propsOnTaskCount; i++)
         {
             hiddenSpot[i].GetComponent<SpriteRenderer>().sprite = propsList[i].silhouette;
             hiddenSpot[i].tag = "Hidden prop";
+            _propsOnTask.Add((prop)propsList[i]);
         }
     }
 
 
     private void generatePropsCurrent()
     {
-        var currentProp = _propsOnLevel[Random.Range(0, _propsOnLevel.Count - 1)];
+        Debug.Log(_propsOnTask);
+        var currentProp = _propsOnTask[Random.Range(0, _propsOnTask.Count - 1)];
         currentSpot.GetComponent<SpriteRenderer>().sprite = currentProp.drawProp;
         _rightSilhouette = currentProp.silhouette;
     }
-
-    //hit.transform.gameObject.SendMessage("OnMouseDown");
-    //hit.transform.gameObject.SendMessage("Clicked",0,SendMessageOptions.DontRequireReceiver);
+        
     private void Update()
     {
         if (_platform == RuntimePlatform.Android || _platform == RuntimePlatform.IPhonePlayer)
@@ -142,7 +165,7 @@ public class levelController : MonoBehaviour
             }
             else
             {
-                lose();
+                loseTask();
             }
         }
     }
@@ -150,7 +173,7 @@ public class levelController : MonoBehaviour
     private void lose()
     {
         Debug.Log("lose");
-        loadLevel();
+        //loadLevel();
     }
 
     private void win()
@@ -159,16 +182,65 @@ public class levelController : MonoBehaviour
         winPanel.SetActive(true);
     }
 
-    private void nextProp()
+    private void clearTask()
     {
-        if (_propsOnLevel.Count > 1)
+        
+    }
+
+    private void nextTask()
+    {
+        _propsOnTask.Clear();
+        _propsOnLevel = _currentLevel.propsOnLevel;
+        shuffle(_propsOnLevel);
+        generatePropsOnHiddenSpots(_propsOnLevel, _currentLevel.propsOnTaskCount);
+        generatePropsCurrent();
+    }
+
+    private void winTask()
+    {
+        _tasksStatusObjects[_taskCurrentNumber].GetComponent<Image>().color = Color.green;
+        if (_taskCurrentNumber < _tasksStatusObjects.Count - 1)
         {
-            _propsOnLevel = _propsOnLevel.Where(prop => prop.silhouette != _rightSilhouette).ToList();
-            generatePropsCurrent();
+            nextTask();
+            _taskCurrentNumber++;
         }
         else
         {
             win();
+        }
+    }
+
+    private void loseTask()
+    {
+        _loseTasksOnLevel++;
+        _tasksStatusObjects[_taskCurrentNumber].GetComponent<Image>().color = Color.red;
+        if (_loseTasksOnLevel == 2)
+        {
+            lose();
+        }
+        else
+        {
+            if (_taskCurrentNumber < _tasksStatusObjects.Count - 1)
+            {
+                nextTask();
+                _taskCurrentNumber++;
+            }else
+            {
+                win();
+            }
+        }
+    }
+
+    private void nextProp()
+    {
+        if (_propsOnTask.Count > 1)
+        {
+            _propsOnTask = _propsOnTask.Where(prop => prop.silhouette != _rightSilhouette).ToList();
+            generatePropsCurrent();
+        }
+        else
+        {
+            winTask();
         }
     }
 
